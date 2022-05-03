77983e0c7beb9b7159188aad55e4128ee90dfb8d
mikaaaa117 <mikaaaa117@gmail.com>
2022-05-03 18:39:27 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          brand: {
+            50: 'rgba(189, 189, 189, 0.5)',
+            100: '#121212'
+          }
+        }
+      },
+    },
+    plugins: [],
+  }
